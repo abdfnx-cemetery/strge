625887a7a3d11b05d575d5f0d33b6248b98fab4a
abdfnx <abdfnabd@gmail.com>
2021-08-20 11:11:36 +0300

    add `OverlayOptionsConfig` & `OptionsConfig` structs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -57,3 +57,53 @@ type ZfsOptionsConfig struct {
 	// Size
 	Size string `toml:"size"`
 }
+
+type OverlayOptionsConfig struct {
+	// IgnoreChownErrors is a flag for whether chown errors should be
+	// ignored when building an image.
+	IgnoreChownErrors string `toml:"ignore_chown_errors"`
+	// MountOpt specifies extra mount options used when mounting
+	MountOpt string `toml:"mountopt"`
+	// Alternative program to use for the mount of the file system
+	MountProgram string `toml:"mount_program"`
+	// Size
+	Size string `toml:"size"`
+	// Do not create a bind mount on the storage home
+	SkipMountHome string `toml:"skip_mount_home"`
+	// ForceMask indicates the permissions mask (e.g. "0755") to use for new
+	// files and directories
+	ForceMask string `toml:"force_mask"`
+}
+
+type OptionsConfig struct {
+	AdditionalImageStores []string `toml:"additionalimagestores"`
+	AdditionalLayerStores []string `toml:"additionallayerstores"`
+	Size string `toml:"size"`
+	RemapUIDs string `toml:"remap-uids"`
+	RemapGIDs string `toml:"remap-gids"`
+	IgnoreChownErrors string `toml:"ignore_chown_errors"`
+	ForceMask os.FileMode `toml:"force_mask"`
+	// RemapUser is the name of one or more entries in /etc/subuid which
+	// should be used to set up default UID mappings.
+	RemapUser string `toml:"remap-user"`
+	RemapGroup string `toml:"remap-group"`
+	// RootAutoUsernsUser is the name of one or more entries in /etc/subuid and
+	// /etc/subgid which should be used to set up automatically a userns.
+	RootAutoUsernsUser string `toml:"root-auto-userns-user"`
+	AutoUsernsMinSize uint32 `toml:"auto-userns-min-size"`
+	AutoUsernsMaxSize uint32 `toml:"auto-userns-max-size"`
+	Aufs struct{ AufsOptionsConfig } `toml:"aufs"`
+	Btrfs struct{ BtrfsOptionsConfig } `toml:"btrfs"`
+	Thinpool struct{ ThinpoolOptionsConfig } `toml:"thinpool"`
+	Overlay struct{ OverlayOptionsConfig } `toml:"overlay"`
+	Vfs struct{ VfsOptionsConfig } `toml:"vfs"`
+	Zfs struct{ ZfsOptionsConfig } `toml:"zfs"`
+	// Do not create a bind mount on the storage home
+	SkipMountHome string `toml:"skip_mount_home"`
+	MountProgram string `toml:"mount_program"`
+	MountOpt string `toml:"mountopt"`
+	// PullOptions specifies options to be handed to pull managers
+	// This API is experimental and can be changed without bumping the major version number.
+	PullOptions map[string]string `toml:"pull_options"`
+	DisableVolatile bool `toml:"disable-volatile"`
+}
