82cc5c2c3c9f39fd7a59f1b2021d77017cb40307
abdfnx <abdfnabd@gmail.com>
2021-08-20 10:28:03 +0300

    finish from `ThinpoolOptionsConfig` struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,4 +5,28 @@ import (
 	"os"
 )
 
-type ThinpoolOptionsConfig struct {}
+type ThinpoolOptionsConfig struct {
+	AutoExtendPercent string `toml:"autoextend_percent"`
+	// AutoExtendThreshold determines the pool extension threshold in terms
+	// of percentage of pool size. For example, if threshold is 60, that
+	// means when pool is 60% full, threshold has been hit.
+	AutoExtendThreshold string `toml:"autoextend_threshold"`
+	BaseSize string `toml:"basesize"`
+	BlockSize string `toml:"blocksize"`
+	DirectLvmDevice string `toml:"directlvm_device"`
+	// DirectLvmDeviceForcewipes device even if device already has a
+	// filesystem
+	DirectLvmDeviceForce string `toml:"directlvm_device_force"`
+	Fs string `toml:"fs"`
+	LogLevel string `toml:"log_level"`
+	// MetadataSize specifies the size of the metadata for the thinpool
+	// It will be used with the `pvcreate --metadata` option.
+	MetadataSize string `toml:"metadatasize"`
+	MinFreeSpace string `toml:"min_free_space"`
+	MkfsArg string `toml:"mkfsarg"`
+	MountOpt string `toml:"mountopt"`
+	Size string `toml:"size"`
+	UseDeferredDeletion string `toml:"use_deferred_deletion"`
+	UseDeferredRemoval string `toml:"use_deferred_removal"`
+	XfsNoSpaceMaxRetries string `toml:"xfs_nospace_max_retries"`
+}
