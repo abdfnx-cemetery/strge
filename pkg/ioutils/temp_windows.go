d716dd87f8c646b57501ce0093e08d9aa4a577ae
abdfnx <abdfnabd@gmail.com>
2021-08-22 12:42:14 +0300

    import `temp` in `ioutils` package

---
+++ pkg/ioutils/temp_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package ioutils
+
+import (
+	"io/ioutil"
+
+	"github.com/gepis/strge/pkg/longpath"
+)
+
+// TempDir is the equivalent of ioutil.TempDir, except that the result is in Windows longpath format.
+func TempDir(dir, prefix string) (string, error) {
+	tempDir, err := ioutil.TempDir(dir, prefix)
+	if err != nil {
+		return "", err
+	}
+
+	return longpath.AddPrefix(tempDir), nil
+}
